4d48a1b1ed29a7533f6dc025c741b6773fa70456
Henny Feliz <mingito.2017@gmail.com>
2024-07-01 23:05:16 -0400

    Message js array file for messages

---
+++ mobile/chat-app/react-chat-app/react-chat-app/src/assets/utils/Messages.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    date: "2023-11-30T09:41:00",
+    sender: "system",
+    text: "This is the main chat template",
+  },
+  { date: "2023-11-30T09:42:00", sender: "user", text: "Oh?" },
+  { date: "2023-11-30T09:43:00", sender: "user", text: "Cool" },
+  { date: "2023-11-30T09:44:00", sender: "user", text: "How does it work?" },
+  {
+    date: "2023-11-30T09:45:00",
+    sender: "system",
+    text: "You just edit any text to type in the conversation you want to show, and delete any bubbles you don’t want to use",
+  },
+  { date: "2023-11-30T09:46:00", sender: "system", text: "Boom!" },
+  { date: "2023-11-30T09:47:00", sender: "user", text: "Hmmm" },
+  { date: "2023-11-30T09:48:00", sender: "user", text: "I think I get it" },
+  {
+    date: "2023-11-30T09:49:00",
+    sender: "user",
+    text: "Will head to the Help Center if I have more questions tho",
+  },
+];
